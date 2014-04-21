a460b81d5129aa9d0d59ce63f75e2acbf068b92d
gbelmonte <gbelmonte@wisc.edu>
2014-04-21 18:53:45 -0500

    Added initial SPI program

---
+++ CC430_Code/cc430x513x_uscia0_spi_09/Debug/subdir_vars.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_cc430f5137.cmd 
+
+C_SRCS += \
+../cc430x513x_uscia0_spi_09.c 
+
+OBJS += \
+./cc430x513x_uscia0_spi_09.obj 
+
+C_DEPS += \
+./cc430x513x_uscia0_spi_09.pp 
+
+C_DEPS__QUOTED += \
+"cc430x513x_uscia0_spi_09.pp" 
+
+OBJS__QUOTED += \
+"cc430x513x_uscia0_spi_09.obj" 
+
+C_SRCS__QUOTED += \
+"../cc430x513x_uscia0_spi_09.c" 
+
+
